9ab44339b97714c29e54f8f7edf88d196cd8c078
Daniel Longacre <danieljlongacre@gmail.com>
2023-07-17 12:07:08 -0400

    Recreate project 2, playing with position, and flex

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 //FOR STRINGS
 var password = "";
-var word = "Daniel";
+var word = "DanielLongacre";
 
 var rand = Math.floor(Math.random() * word.length);
-console.log(rand);
 password += word.substring(rand, rand + 1);
-
-
 console.log(password);
 
 
@@ -15,22 +12,35 @@ var password2 = [];
 var arr1 = ["apple", "banana", "orange", "grape"];
 
 var rand2 = Math.floor(Math.random() * arr1.length);
-console.log(rand2);
 password2 += arr1[rand2];
 
 console.log(password2);
 
+//-----------------------------------------------------------------------------
 
-//Example
+
+//Example1
 var combo = [];
 var vegetables = ["Potato", "Broccoli", "Spinach", "Beet", "Corn", "Peas"];
 console.log(vegetables);
-console.log(vegetables.length);
 
 for(var i = 0; i < 3; i++) {
     var rand = Math.floor(Math.random() * vegetables.length);
     combo[i] = vegetables[rand];
+    vegetables.splice(rand, 1);
 }
 
 console.log(combo);
-console.log(combo.length);
+
+//-----------------------------------------------------------------------------
+
+//Example2
+var pass = [];
+var numArr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+for(var i = 0; i < 5; i++) {
+    var rand = Math.floor(Math.random() * numArr.length);
+    pass[i] = numArr[rand];
+    numArr.splice(rand, 1);
+}
+console.log(pass);
